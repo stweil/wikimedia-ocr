5505ee47b6c1dd6f881709995cb2351d4899064e
Stefan Weil <sw@weilnetz.de>
2023-08-14 21:30:45 +0200

    Add new OCR engine kraken
    Kraken is an Open Source OCR engine with trainable segmentation and
    OCR models. It can work with printed and handwritten texts.
    
    This initial implementation comes with two generic OCR models which
    can be used on a wide range of German publications, but also with
    other languages which are based on Latin script.
    
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

--- src/Controller/OcrController.php
+++ src/Controller/OcrController.php
@@ -9,6 +9,7 @@ use App\Engine\EngineBase;
 use App\Engine\EngineFactory;
 use App\Engine\EngineResult;
 use App\Engine\GoogleCloudVisionEngine;
+use App\Engine\KrakenEngine;
 use App\Engine\TesseractEngine;
 use App\Engine\TranskribusEngine;
 use App\Exception\EngineNotFoundException;
@@ -32,7 +33,7 @@ class OcrController extends AbstractController {
 	/** @var Intuition */
 	protected $intuition;
 
-	/** @var TesseractEngine|GoogleCloudVisionEngine|TranskribusEngine */
+	/** @var KrakenEngine|TesseractEngine|GoogleCloudVisionEngine|TranskribusEngine */
 	protected $engine;
 
 	/** @var string The default OCR engine. */
@@ -134,6 +135,8 @@ class OcrController extends AbstractController {
 		// because we want the default set if the user changes the engine to Transkribus.
 		static::$params['line_id'] = (int)$this->request->query->get( 'line_id', (string)static::$params['line_id'] );
 
+		// Apply the kraken-specific settings
+
 		// Apply the tesseract-specific settings
 		// NOTE: Intentionally excluding `oem`, see T285262
 		if ( TesseractEngine::getId() === static::$params['engine'] ) {
@@ -217,7 +220,7 @@ class OcrController extends AbstractController {
 	 * @OA\Parameter(
 	 *     name="engine",
 	 *     in="query",
-	 *     description="The engine to use, either `tesseract` or `google` or `transkribus`.",
+	 *     description="The engine to use, either `kraken` or `tesseract` or `google` or `transkribus`.",
 	 *     example="tesseract",
 	 * @OA\Schema(type="string")
 	 * )
@@ -301,7 +304,7 @@ class OcrController extends AbstractController {
 	 * @OA\Parameter(
 	 *     name="engine",
 	 *     in="query",
-	 *     description="The engine to use, either `tesseract` or `google` or `transkribus`.",
+	 *     description="The engine to use, either `kraken` or `tesseract` or `google` or `transkribus`.",
 	 *     example="tesseract",
 	 * @OA\Schema(type="string")
 	 * )
